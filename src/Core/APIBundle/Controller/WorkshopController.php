f3ce6aeb13df898f0d079a6932ae3c7b9db9acc0
ljb <leon.bergmann@sky-lab.de>
2016-04-30 11:12:33 +0200

    some improvements at API & Entitys

---
+++ src/Core/APIBundle/Controller/WorkshopController.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Leon Bergmann
+ * Date: 29.04.2016
+ * Time: 16:44
+ */
+namespace Core\APIBundle\Controller;
+
+use Doctrine\Common\Collections\Criteria;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Request\ParamFetcher;
+use FOS\RestBundle\View\View;
+use JMS\Serializer\SerializationContext;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Routing\ClassResourceInterface;
+use FOS\RestBundle\Controller\Annotations\RouteResource;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\Query;
+
+
+class WorkshopController extends FOSRestController implements ClassResourceInterface
+{
+    /**
+     * @ApiDoc(
+     *  resource=true,
+     *  description="Returns list of all Workshops that are active",
+     *  statusCodes = {
+     *      200 = "Returned when successful",
+     *      405 = "Returned when the data is not found"
+     *  }
+     * )
+     * )
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Rest\View()
+     */
+    public function cgetAction()
+    {
+        $workshopRepo = $this->getDoctrine()->getManager()->getRepository('CoreEntityBundle:Workshop');
+        $entits = $workshopRepo->getAllActiveWorkshops();
+        if (!$entits) {
+            throw $this->createNotFoundException("No Workshops found");
+        }
+
+        $view = $this->view("{'Hallo'}", 200);
+        return $this->handleView($view);
+    }
+
+    public function getDetailAction($id)
+    {
+
+    }
+
+}
