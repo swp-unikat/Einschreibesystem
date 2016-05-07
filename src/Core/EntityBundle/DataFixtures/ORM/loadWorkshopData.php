f90d6ee0d2535e49614d8b871036c37ad381d5a6
Leon Bergmann <leon.bergmann@sky-lab.de>
2016-05-07 21:18:42 +0200

    user \DateTime()

--- src/Core/EntityBundle/DataFixtures/ORM/loadWorkshopData.php
+++ src/Core/EntityBundle/DataFixtures/ORM/loadWorkshopData.php
@@ -20,15 +20,15 @@ class loadWorkshopData extends AbstractFixture implements OrderedFixtureInterfac
      * @var ObjectManager
      */
     protected $manager;
-    
+
     public function load(ObjectManager $manager){
         $workshop = new Workshop();
         $workshop->setTitle("Grundlagen Löten Level 0 - MitarbeiterSpecial ");
         $workshop->setCost(10.00);
         $workshop->setLocation("Haus M, Raum 606");
         $workshop->setMaxParticipants(12);
-        $workshop->setEndAt(strtotime("23.05.2016 21:00:00"));
-        $workshop->setStartAt(strtotime("23.05.2016 19:00:00"));
+        $workshop->setEndAt(new \DateTime("23.05.2016 21:00:00"));
+        $workshop->setStartAt(new \DateTime("23.05.2016 19:00:00"));
         $workshop->setRequirements("");
         $workshop->setDescription("In unserem Level 0 Lötworkshop vermitteln wir alle nötigen Grundlagen zum Umgang mit dem Lötkolben. Dabei erklären wir, worauf es beim Löten ankommt, geben Tipps und helfen die Unsicherheit vieler Anfänger zu nehmen. Dieser Workshop richtet sich an jeden interessierten Mitarbeiter aller Fakultäten und Verwaltungen der TU Ilmenau.");
         $manager->persist($workshop);
@@ -38,8 +38,8 @@ class loadWorkshopData extends AbstractFixture implements OrderedFixtureInterfac
         $workshop->setCost(4.00);
         $workshop->setLocation("Haus M, Raum 606");
         $workshop->setMaxParticipants(5);
-        $workshop->setEndAt(strtotime("20.05.2016 21:00:00"));
-        $workshop->setStartAt(strtotime("20.05.2016 19:00:00"));
+        $workshop->setEndAt(new \DateTime("20.05.2016 21:00:00"));
+        $workshop->setStartAt(new \DateTime("20.05.2016 19:00:00"));
         $workshop->setRequirements("Wenn möglich, bitte eine Wäscheklammer mitbringen!");
         $workshop->setDescription("In diesem Workshop bekommst du die Möglichkeit, dir deinen eigenen optischen Pulsmesser zu bauen. Du lernst, wie man eine typische Signalkette mit Sensorik und Verstärkung aufbaut, grundlegende Filterschaltungen und wie eigentlich Operationsverstärker wirklich aussehen. Am Ende des Workshops hast du deinen eigenen Fitnessclip in der Hand, welcher rein analog mit Hilfe einer LED deinen Puls anzeigt.");
         $manager->persist($workshop);
@@ -49,8 +49,8 @@ class loadWorkshopData extends AbstractFixture implements OrderedFixtureInterfac
         $workshop->setCost(10.00);
         $workshop->setLocation("Haus M, Raum 606");
         $workshop->setMaxParticipants(5);
-        $workshop->setEndAt(strtotime("27.05.2016 21:00:00"));
-        $workshop->setStartAt(strtotime("27.05.2016 19:00:00"));
+        $workshop->setEndAt(new \DateTime("27.05.2016 21:00:00"));
+        $workshop->setStartAt(new \DateTime("27.05.2016 19:00:00"));
         $workshop->setRequirements("Wenn möglich, bitte eine Wäscheklammer mitbringen!");
         $workshop->setDescription("In diesem Workshop bekommst du die Möglichkeit, dir deinen eigenen optischen Pulsmesser zu bauen. Du lernst, wie man eine typische Signalkette mit Sensorik und Verstärkung aufbaut, grundlegende Filterschaltungen und wie eigentlich Operationsverstärker wirklich aussehen. Am Ende des Workshops hast du deinen eigenen Fitnessclip in der Hand, welcher rein analog mit Hilfe einer LED deinen Puls anzeigt.");
         $manager->persist($workshop);
