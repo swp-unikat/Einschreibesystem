f55d28e06dc8e23c90eee4f8b0322650d8bd0688
Leon Bergmann <development@sky-lab.de>
2016-05-09 14:21:08 +0200

    some fixes

--- src/Core/EntityBundle/DataFixtures/ORM/loadParticipantsData.php
+++ src/Core/EntityBundle/DataFixtures/ORM/loadParticipantsData.php
@@ -13,37 +13,53 @@ use Core\EntityBundle\Entity\Participants;
 
 class loadParticipantsData implements FixtureInterface
 {
+    /**
+     * @var ObjectManager
+     */
+    protected $manager;
+    
     public function load(ObjectManager $manager){
         $participant = new Participants();
         $participant->setName("Bergmann");
         $participant->setSurname("Leon");
         $participant->setEmail("leon.bergmann@tu-ilmenau.de");
+        $participant->setBlacklisted(false);
         $manager->persist($participant);
+        
         $participant2 = new Participants();
         $participant2->setName("Hanisch");
         $participant2->setSurname("Marco");
         $participant2->setEmail("marco.hanisch@tu-ilmenau.de");
-        $Manager->persist($participant2);
+        $participant2->setBlacklisted(false);
+        $manager->persist($participant2);
+        
         $participant3 = new Participants();
         $participant3->setName("Griebel");
         $participant3->setSurname("Martin");
         $participant3->setEmail("martin.griebel@tu-ilmenau.de");
-        $Manager->persist($participant3);
+        $participant3->setBlacklisted(false);
+        $manager->persist($participant3);
+        
         $participant4 = new Participants();
         $participant4->setName("Durak");
         $participant4->setSurname("Ahmet");
         $participant4->setEmail("ahmet.durak@tu-ilmenau.de");
-        $Manager->persist($participant4);
+        $participant4->setBlacklisted(false);
+        $manager->persist($participant4);
+        
         $participant5 = new Participants();
         $participant5->setName("Heringklee");
         $participant5->setSurname("Stefan");
         $participant5->setEmail("stefan.heringklee@tu-ilmenau.de");
-        $Manager->persist($participant5);
+        $participant5->setBlacklisted(false);
+        $manager->persist($participant5);
+        
         $participant6 = new Participants();
         $participant6->setName("Schaefer");
         $participant6->setSurname("Valentin");
         $participant6->setEmail("valentin.schaefer@tu-ilmenau.de");
-        $Manager->persist($participant6);
+        $participant6->setBlacklisted(false);
+        $manager->persist($participant6);
         /*
          * Hier weitere Teilneher anlegen
          */
