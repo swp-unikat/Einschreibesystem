3707c607117b8d040a53dd4952b9500d6e64a4a3
MarcoHanisch <marco.hanisch@tu-ilmenau.de>
2016-05-09 20:54:41 +0200

    Update ParticipantsController.php
    fixed #57 mh

--- src/Core/APIBundle/Controller/ParticipantsController.php
+++ src/Core/APIBundle/Controller/ParticipantsController.php
@@ -13,7 +13,6 @@ use FOS\RestBundle\Request\ParamFetcher;
 use FOS\RestBundle\View\View;
 use JMS\Serializer\SerializationContext;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use FOS\RestBundle\Controller\Annotations as Rest;
