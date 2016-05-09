b836891dc2c6f57a21a8a3598bb890a559ce2763
MartinG95 <martin.griebel@yahoo.de>
2016-05-09 21:34:47 +0200

    Update WorkshopController.php

--- src/Core/APIBundle/Controller/WorkshopController.php
+++ src/Core/APIBundle/Controller/WorkshopController.php
@@ -19,7 +19,6 @@ use Core\EntityBundle\Entity\WorkshopParticipants;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use FOS\RestBundle\Controller\Annotations as Rest;
