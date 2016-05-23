a045337b7dff50fe4ac7ea6f159a6201573c5266
Stefan Heringklee <stefan.heringklee@tu-ilmenau.de>
2016-05-23 14:47:05 +0200

    updated view login

--- web/components/controllers/dashboardCtrl.js
+++ web/components/controllers/dashboardCtrl.js
@@ -5,7 +5,7 @@ var mainAppCtrls = angular.module("mainAppCtrls");
 //TODO: if /dashboard is called, change hideDashboard to false
 mainAppCtrls.controller('DashboardCtrl',['$scope',
     function($scope) {
-        $scope.hideDashboard = false;
+
     }
 
 ]);
