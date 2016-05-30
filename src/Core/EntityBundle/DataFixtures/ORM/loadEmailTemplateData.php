7604e3d9f9ae994a1571813256b12fc689f4b186
Leon Bergmann <leon.bergmann@sky-lab.de>
2016-05-30 13:45:37 +0200

    fixed some minor errors at EmailTempalte

--- src/Core/EntityBundle/DataFixtures/ORM/loadEmailTemplateData.php
+++ src/Core/EntityBundle/DataFixtures/ORM/loadEmailTemplateData.php
@@ -18,25 +18,25 @@ class loadEmailTemplate implements FixtureInterface
     protected $manager;
     public function load(ObjectManager $manager){
     $emailtemplate = new EmailTemplate();
-    $emailtemplate->setName("Erinnerungsmail");
+    $emailtemplate->setTemplateName("Erinnerungsmail");
     $emailtemplate->setEmailSubject("Erinnerungsmail für ausstehenden Workshop");
     $emailtemplate->setEmailBody("Zur Erinnerung: Morgen um 15 Uhr beginnt der Workshop Löten, für den Sie sich eingeschrieben haben!");
     $manager->persist($emailtemplate);
     
     $emailtemplate2 = new EmailTemplate();
-    $emailtemplate2->setName("Zahlungsaufforderung");
+    $emailtemplate2->setTemplateName("Zahlungsaufforderung");
     $emailtemplate2->setEmailSubject("Zahlungsaufforderung für Workshop");
     $emailtemplate2->setEmailBody("Sie haben bislang noch keine Zahlung für den Workshop getätigt! Wir bitten Sie dies innerhalb der nächsten 4 Tage zutun.");
     $manager->persist($emailtemplate2);
     
     $emailtemplate3 = new EmailTemplate();
-    $emailtemplate3->setName("Ausfall");
+    $emailtemplate3->setTemplateName("Ausfall");
     $emailtemplate3->setEmailSubject("Ausfall eines ausstehenden Workshops");
     $emailtemplate3->setEmailBody("Der Workshop in 2 Tagen kann aufgrund von Krankheit nicht stattfinden! Die Kosten bekommen Sie selbst verständlich erstattet.");
     $manager->persist($emailtemplate3);
     
     $emailtemplate4 = new EmailTemplate();
-    $emailtemplate4->setName("Teilnahmebestätigung");
+    $emailtemplate4->setTemplateName("Teilnahmebestätigung");
     $emailtemplate4->setEmailSubject("Teilnahmebestätigung für absolvierten Workshop");
     $emailtemplate4->setEmailBody("Im Anhang dieser Mail finden Sie eine Teilnahmebestätigung, an dem Sie erfolgreich teilgenommen haben!");
     $manager->persist($emailtemplate4);
