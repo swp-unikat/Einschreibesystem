4da9f26c327f79ba715aef5093d192aa073cba96
vallesch <valentin.schaefer@tu-ilmenau.de>
2016-05-31 15:37:32 +0200

    updated rest service

--- web/components/services/restService.js
+++ web/components/services/restService.js
@@ -3,7 +3,8 @@ var restSvcs = angular.module('restSvcs',['ngResource']);
  * Service consuming API-function regarding workshops
  */
 restSvcs.factory('Workshops',['$resource',function($resource){
-    return $resource('/api/workshops/all',{},{
-        getAll: {method: 'GET', isArray: true}
+    return $resource('/api/workshops/:id',{id: '@id'},{
+        getAll: {method: 'GET', isArray: true,id: 'all'},
+        get: {method: 'GET', isArray: false,id: '@id'}
     });
 }]);
