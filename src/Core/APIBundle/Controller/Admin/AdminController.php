9fcc074a4bb74adb6ab59b3a5f8c8f600d39fab9
MarcoHanisch <marco.hanisch@tu-ilmenau.de>
2016-05-31 15:02:19 +0200

    Update AdminController.php
    putaction

--- src/Core/APIBundle/Controller/Admin/AdminController.php
+++ src/Core/APIBundle/Controller/Admin/AdminController.php
@@ -8,3 +8,27 @@
  */
 namespace Core\APIBundle\Controller\Admin;
 
+/**
+ * Class RestController.
+ */
+ 
+ class AdminController extends FOSRestController implements ClassResourceInterface
+{	/**
+     * @ApiDoc(
+     *  resource=true,
+     *  description="Action to create new Admin",
+     *  output = "Core\EntityBundle\Entity\Admin",
+     *  statusCodes = {
+     *      200 = "Returned when successful",
+     *      404 = "Returned when the data is not found"
+     *  }
+     * )
+     * )
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Rest\View()
+     */  
+     public function putAction()
+     {
+     }
+     
