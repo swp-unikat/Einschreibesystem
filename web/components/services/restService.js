d8dc08911ac714382a66b497132b6f4f9e8f1faf
MarcoHanisch <marco.hanisch@tu-ilmenau.de>
2016-06-03 22:53:21 +0200

    Update restService.js
    restSvcs.factory('WorkshopsTemplate'

--- web/components/services/restService.js
+++ web/components/services/restService.js
@@ -43,7 +43,7 @@ restSvcs.factory('Workshops',['$resource',function($resource){
          * @methodOf restSvcs.Workshops
          * @param {integer} id Workshop-ID
          */
-        'post': {method: 'POST',url:'/api/workshops/:id/enrolls',params: {id: '@id'},isArray: true},
+        'post': {method: 'POST',url:'/api/workshops/:id/enrolls',params: {id: '@id'},isArray: false},
         /**
          * @ngdoc funtion
          * @name restSvcs.Workshops#unsubscribeWorkshop
@@ -52,7 +52,7 @@ restSvcs.factory('Workshops',['$resource',function($resource){
          * @param {integer} id Workshop-ID
          * @param {string} token Unsubscribetoken
          */
-        'getUnsubscribes': {method: 'GET',url:'/api/workshops/:id/unsubscribes/:token',params: {id: '@id', token: '@token'},isArray: true},
+        'getUnsubscribes': {method: 'GET',url:'/api/workshops/:id/unsubscribes/:token',params: {id: '@id', token: '@token'},isArray: false},
         /**
          * @ngdoc funtion
          * @name restSvcs.Workshops#unsubscribeWorkshop
@@ -60,7 +60,8 @@ restSvcs.factory('Workshops',['$resource',function($resource){
          * @methodOf restSvcs.Workshops
          * @param {integer} id Workshop-ID
          */
-        'getWaitinglist': {method: 'GET',url:'/api/workshops/:id/waitinglist',params: {id: '@id'},isArray: true},/**
+        'getWaitinglist': {method: 'GET',url:'/api/workshops/:id/waitinglist',params: {id: '@id'},isArray: true},
+        /**
          * @ngdoc funtion
          * @name restSvcs.Workshops#unsubscribeWorkshop
          * @description Confirm Enrollment of the WOrkshop
@@ -68,6 +69,54 @@ restSvcs.factory('Workshops',['$resource',function($resource){
          * @param {integer} id Workshop-ID, participantsid Participants-ID
          * @param {string} token Confirmtoken
          */
-        'getConfirmEnrollment': {method: 'GET',url:'/api/workshops/:id/enrolls/:participantsid/confirms/:token',params: {id: '@id',participantsid: '@participantsid',token: '@token'},isArray: true},
+        'getConfirmEnrollment': {method: 'GET',url:'/api/workshops/:id/enrolls/:participantsid/confirms/:token',params: {id: '@id',participantsid: '@participantsid',token: '@token'},isArray: false},
+    });
+}]);
+/**
+ * @ngdoc service
+ * @name restSvcs.WorkshopTemplate
+ * @description Provides CRUD operations for Workshop-Template-functions provided by the API
+ */
+restSvcs.factory('WorkshopsTemplate',['$resource',function($resource){
+    return $resource('/api/admin/workshops/templates/:id',{},{
+        /**
+         * @ngdoc funtion
+         * @name restSvcs.WorkshopTemplate#getAll
+         * @description get a list of all currently available workshoptemplates.
+         * @methodOf restSvcs.Workshoptemplate
+         * @returns {httpPromise} resolve with fetched data, or fails with error description.
+         */
+        'getAll': {method: 'GET',params: {id: 'all'}, isArray: true},
+        /**
+         * @ngdoc funtion
+         * @name restSvcs.WorkshopTemplate#get
+         * @description get a single workshoptemplate
+         * @methodOf restSvcs.Workshoptemplate
+         * @param {integer} id Workshop-ID
+         */
+        'get': {method: 'GET',params: {id: '@id'}, isArray: false},
+        /**
+         * @ngdoc function
+         * @name restSvcs.WorkshopTemplate#patch
+         * @description edit a single workshoptemplate
+         * @methodOf restSvcs.Workshoptemplate
+         * @param {integer} id Workshop-ID
+         */
+        'patch': {method: 'PATCH',params: {id: '@id'}, isArray: false},
+        /**
+         * @ngdoc function
+         * @name restSvcs.WorkshopTemplate#put
+         * @description create a new workshoptemplate
+         * @methodOf restSvcs.Workshoptemplate
+         */
+         'put': {method: 'PUT', isArray: false},
+         /**
+          * @ngdoc function
+          * @name restSvcs.WorkshopTemplate#delete
+          * @description delete a workshoptemplate
+          * @methodOf restSvcs.Workshoptemplate
+          * @param {integer} id Workshop-ID
+          */
+          'delete': {method: 'DELETE',params: {id: '@id'}, isArray: false}
     });
 }]);
