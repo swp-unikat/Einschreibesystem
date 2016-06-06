84e50059dcde05183a125c6b207584d506dc3061
Mohammad <mohammad-ahmed-jaafar.hamdar@tu-ilmenau.de>
2016-06-06 17:34:44 +0200

    Updated printService.js

--- web/components/services/printService.js
+++ web/components/services/printService.js
@@ -6,22 +6,14 @@ var prntSvcs = angular.module("prntSvcs",['$rootScope', '$compile','$http','$tim
 
 prntSvcs.factory('printer',['printer', function($rootScope, $compile, $http, $timeout ){
 
-    var print = function (templateUrl, data) {
-        $http.get(templateUrl).success(function(template){
-            var printScope = $rootScope.$new()
-            angular.extend(printScope, data);
-            var element = $compile($('<div>' + template + '</div>'))(printScope);
-            var waitForRenderAndPrint = function() {
-                if(printScope.$$phase || $http.pendingRequests.length) {
-                    $timeout(waitForRenderAndPrint);
-                } else {
-                    // Replace printHtml with openNewWindow for debugging
-                    printHtml(element.html());
-                    printScope.$destroy();
-                }
-            };
-            waitForRenderAndPrint();
-        });
+    var printHtml = function (html) {
+        var deferred = $q.defer();
+        var hiddenFrame = $('<iframe style="display: none"></iframe>').appendTo('body')[0];
+        hiddenFrame.contentWindow.printAndRemove = function() {
+            hiddenFrame.contentWindow.print();
+            $(hiddenFrame).remove();
+        };
+        
     };
 
 
