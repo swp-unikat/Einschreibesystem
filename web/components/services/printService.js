2f4b1f2bd54a2225f2e07d87a253f9e5172601ef
Mohammad <mohammad-ahmed-jaafar.hamdar@tu-ilmenau.de>
2016-06-06 17:49:37 +0200

    Updated printService.js

--- web/components/services/printService.js
+++ web/components/services/printService.js
@@ -14,8 +14,23 @@ prntSvcs.factory('printer',['printer', function($rootScope, $compile, $http, $ti
         };
 
     };
+    var print = function (templateUrl, data) {
+        $http.get(templateUrl).success(function(template){
+            var printScope = angular.extend($rootScope.$new(), data);
+            var element = $compile($('<div>' + template + '</div>'))(printScope);
+            var waitForRenderAndPrint = function() {
+                if(printScope.$$phase || $http.pendingRequests.length) {
+                    $timeout(waitForRenderAndPrint);
+                } else {
+                    printHtml(element.html());
+                    printScope.$destroy(); // To avoid memory leaks from scope create by $rootScope.$new()
+                }
+            }
+            waitForRenderAndPrint();
+        });
+    };
 
-    
+
 }]);
 
 
