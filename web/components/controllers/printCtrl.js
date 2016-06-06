8160fcf1033ebdec53f70ddcb2e846b585d2eddc
Mohammad <mohammad-ahmed-jaafar.hamdar@tu-ilmenau.de>
2016-06-06 13:26:46 +0200

    Created printCtrl.js

---
+++ web/components/controllers/printCtrl.js
@@ -0,0 +1,13 @@
+/**
+ * Created by hunte on 05/06/2016.
+ */
+
+var mainAppCtrls = angular.module("mainAppCtrls");
+
+mainAppCtrls.controller('PrintCtrl',['$scope',
+    function($scope) {
+
+    }
+
+    ]);
+    
