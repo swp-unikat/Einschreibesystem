e2e8f38ec7bb1804222574f04c07a5fb86326639
Mohammad <mohammad-ahmed-jaafar.hamdar@tu-ilmenau.de>
2016-06-06 13:36:44 +0200

    Updated printCtrl.js

--- web/components/controllers/printCtrl.js
+++ web/components/controllers/printCtrl.js
@@ -6,7 +6,23 @@ var mainAppCtrls = angular.module("mainAppCtrls");
 
 mainAppCtrls.controller('PrintCtrl',['$scope',
     function($scope) {
-
+        var print = function (templateUrl, data) {
+            $http.get(templateUrl).success(function(template){
+                var printScope = $rootScope.$new()
+                angular.extend(printScope, data);
+                var element = $compile($('<div>' + template + '</div>'))(printScope);
+                var waitForRenderAndPrint = function() {
+                    if(printScope.$$phase || $http.pendingRequests.length) {
+                        $timeout(waitForRenderAndPrint);
+                    } else {
+                        // Replace printHtml with openNewWindow for debugging
+                        printHtml(element.html());
+                        printScope.$destroy();
+                    }
+                };
+                waitForRenderAndPrint();
+            });
+        };
     }
 
     ]);
