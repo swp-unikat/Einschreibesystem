527f44345a6cb58607cf18df2de21535cae235bc
Mohammad <mohammad-ahmed-jaafar.hamdar@tu-ilmenau.de>
2016-06-06 17:27:19 +0200

    Updated printService.js

--- web/components/services/printService.js
+++ web/components/services/printService.js
@@ -1,7 +1,31 @@
 /**
  * Created by hunte on 06/06/2016.
  */
-var mainAppCtrls = angular.module("mainAppCtrls");
-mainAppCtrls.factory('serviceId', function() {
-    
-});
+
+var prntSvcs = angular.module("prntSvcs",['$rootScope', '$compile','$http','$timeout']);
+
+prntSvcs.factory('printer',['printer', function($rootScope, $compile, $http, $timeout ){
+
+    var print = function (templateUrl, data) {
+        $http.get(templateUrl).success(function(template){
+            var printScope = $rootScope.$new()
+            angular.extend(printScope, data);
+            var element = $compile($('<div>' + template + '</div>'))(printScope);
+            var waitForRenderAndPrint = function() {
+                if(printScope.$$phase || $http.pendingRequests.length) {
+                    $timeout(waitForRenderAndPrint);
+                } else {
+                    // Replace printHtml with openNewWindow for debugging
+                    printHtml(element.html());
+                    printScope.$destroy();
+                }
+            };
+            waitForRenderAndPrint();
+        });
+    };
+
+
+
+}]);
+
+
