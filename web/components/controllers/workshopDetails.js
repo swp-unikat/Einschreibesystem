85254a830a3ec3ba60f5a511dcfed452f4e72f25
AhmetDurak7777 <ahmet.durak@tu-ilmenau.de>
2016-06-07 17:59:02 +0200

    updated adminNewWorkshopTemplate.html updated adminWorkshopTemplate.html updated workshopDetails.html updated workshopDetails.js updated lang-en.json

--- web/components/controllers/workshopDetails.js
+++ web/components/controllers/workshopDetails.js
@@ -2,10 +2,26 @@
  * Created by Ahmet on 31.05.2016.
  */
 var mainAppCtrls = angular.module("mainAppCtrls");
-mainAppCtrls.controller('WorkshopDetailsCtrl',['$scope','Workshops', '$stateParams',
-    function($scope,Workshops,$stateParams) {
+mainAppCtrls.controller('WorkshopDetailsCtrl',['$scope','Workshops', '$stateParams', "$alert",
+    function($scope,Workshops,$stateParams, $alert) {
         //TODO : replace with workshop details
         var workshopid;
+        $scope.sendInfo= function(){
+            var first_name=$scope.first_name;   
+            var last_name=$scope.last_name;
+            var email=$scope.e_mail;
+
+            if(!email.$valid){
+              alert("testiii");
+            }
+            if(!first_name){
+
+            }
+            if(!last_name){
+
+            }
+        };
+
         workshopid = $stateParams.id;
         $scope.loading = true;
         Workshops.get({id: workshopid}).$promise.then(function(value,httpResponse){
