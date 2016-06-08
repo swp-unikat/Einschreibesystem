1a02c0616ca500d895ba9be46358b986bf0a9a8a
Mohammad <mohammad-ahmed-jaafar.hamdar@tu-ilmenau.de>
2016-06-08 11:45:34 +0200

    Updated Print Service

--- web/components/services/printService.js
+++ web/components/services/printService.js
@@ -2,9 +2,9 @@
  * Created by hunte on 06/06/2016.
  */
 
-var prntSvcs = angular.module("prntSvcs",['$rootScope','$compile','$http','$timeout']);
+var prntSvcs = angular.module("prntSvcs",[]);
 
-prntSvcs.factory('printer',['printer', function($rootScope, $compile, $http, $timeout ){
+prntSvcs.factory('printer',['$rootScope','$compile','$http','$timeout', function($rootScope, $compile, $http, $timeout ){
     var printHtml = function (html) {
         var deferred = $q.defer();
         var hiddenFrame = $('<iframe style="display: none"></iframe>').appendTo('body')[0];
