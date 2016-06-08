cac5e80417fe8a714b000b79e5672eb67d547bd7
Stefan Heringklee <stefan.heringklee@tu-ilmenau.de>
2016-06-08 11:35:09 +0200

    updated editEmailTemplateCtrl.js updated ctrl.js

--- web/components/dist/ctrl.js
+++ web/components/dist/ctrl.js
@@ -97,6 +97,60 @@ mainAppCtrls.controller('adminWorkshopDetailsCtrl',['$scope','Workshops', '$stat
     }
 ])
 
+// Source: web/components/controllers/adminWorkshopManagementCtrl.js
+/**
+ * Created by Ahmet on 08.06.2016.
+ */
+
+/**
+ *
+ */
+mainAppCtrls.controller('adminWorkshopManagementCtrl',['$scope','Workshops','$alert','$translate',
+    function($scope,Workshops,$alert,$translate) {
+
+        //Define object to store the alert in
+        $scope.myAlert;
+
+        //Get and store translation for alert title.
+        $translate(['TITLE_ERROR', 'ERROR_NO_WORKSHOPS']).then(function (translations) {
+            $scope.errorTitle = translations.TITLE_ERROR;
+            $scope.errorMsg = translations.ERROR_NO_WORKSHOPS;
+        });
+        $scope.loading = true;
+        Workshops.getAll().$promise.then(function(value){
+            $scope.workshopList = value;
+            $scope.loading = false;
+        },function(httpResponse) {
+            //switch through all possible errors
+            switch(httpResponse.status){
+                //Alert for error 404, no workshops available
+                case 404:
+                    $scope.myAlert = $alert({
+
+                        title: $scope.errorTitle,
+                        type: 'danger',
+                        content: $scope.errorMsg,
+                        container: '#alert',
+                        dismissable: false,
+                        show: true
+                    });
+                case 500:
+                    $scope.myAlert = $alert({
+                        title: $scope.errorTitle,
+                        type: 'danger',
+                        content: 'Internal server error.',
+                        container: '#alert',
+                        dismissable: false,
+                        show: true
+                    })
+                    break;
+            }
+            $scope.loading = false;
+        });
+
+    }
+]);
+
 // Source: web/components/controllers/administratorManagementCtrl.js
 /**
  * Created by hunte on 31/05/2016.
@@ -143,8 +197,17 @@ mainAppCtrls.controller('ContactCtrl',['$scope',
 // Source: web/components/controllers/editEmailTemplateCtrl.js
 /**
  * Created by hunte on 31/05/2016.
- */
+*/
 
+/**
+ *
+ */
+mainAppCtrls.controller('EditEmailTemplateCtrl',['$scope',
+    function($scope) {
+
+    }
+
+]);
 
 // Source: web/components/controllers/editWorkshopTemplateCtrl.js
 /**
