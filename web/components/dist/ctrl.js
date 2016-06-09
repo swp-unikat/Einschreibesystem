4063d50c823ca78162387f715484a2cc0177c99a
AhmetDurak7777 <ahmet.durak@tu-ilmenau.de>
2016-06-09 09:38:16 +0200

    updated adminNewWorkshopTemplate.html updated adminWorkshopTemplate.html updated newWorkshopTemplateCtrl.js

--- web/components/dist/ctrl.js
+++ web/components/dist/ctrl.js
@@ -300,8 +300,27 @@ mainAppCtrls.controller('NewEmailTemplateCtrl',['$scope',
 /**
  *
  */
-mainAppCtrls.controller('NewWorkshopTemplateCtrl',['$scope',
-    function($scope) {
+mainAppCtrls.controller('NewWorkshopTemplateCtrl',['$scope',"WorkshopTemplate",
+    function($scope, WorkshopTemplate) {
+
+        $scope.sendInfo = function(){
+            var data={
+                title:$scope.workshop.title,
+                description:$scope.workshop.description,
+                cost:$scope.workshop.cost,
+                requirements:$scope.workshop.requirement,
+                location:$scope.workshop.location,
+                start_at:$scope.sharedDate,
+                end_at:JSON.stringify(new Date(2016,10,10,10,10,0,0)),
+                max_participants: 15
+            }
+            WorkshopTemplate.putWorkshopTemplate(data).$promise.then(function(value){
+                alert('Success!');
+            },function(httpResponse){
+                alert('Error'+httpResponse.statusText);
+            });
+        }
+
 
     }
 
