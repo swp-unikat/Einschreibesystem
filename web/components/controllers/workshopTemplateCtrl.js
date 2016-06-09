fd1a83e26f1bc7cbe0f0cf05f1478cf92a30bd0b
AhmetDurak7777 <ahmet.durak@tu-ilmenau.de>
2016-06-09 21:28:18 +0200

    Merge branch 'frontend' of C:\Users\Ahmet\Projects\Einschreibesystem with conflicts.

--- web/components/controllers/workshopTemplateCtrl.js
+++ web/components/controllers/workshopTemplateCtrl.js
@@ -5,9 +5,49 @@ var mainAppCtrls = angular.module("mainAppCtrls");
 /**
  *
  */
-mainAppCtrls.controller('WorkshopTemplateCtrl',['$scope',
-    function($scope) {
+mainAppCtrls.controller('WorkshopTemplateCtrl',['$scope',"WorkshopTemplate","$alert","$translate",
+    function($scope,WorkshopTemplate,$alert,$translate) {
+        //Define object to store the alert in
+        $scope.myAlert;
+
+        //Get and store translation for alert title.
+        $translate(['TITLE_ERROR', 'ERROR_NO_WORKSHOPS']).then(function (translations) {
+            $scope.errorTitle = translations.TITLE_ERROR;
+            $scope.errorMsg = translations.ERROR_NO_WORKSHOPS;
+        });
+        $scope.loading = true;
+        WorkshopTemplate.getAll().$promise.then(function(value){
+            $scope.workshopList = value;
+            $scope.loading = false;
+        },function(httpResponse) {
+            //switch through all possible errors
+            switch(httpResponse.status){
+                //Alert for error 404, no workshops available
+                case 404:
+                    $scope.myAlert = $alert({
+
+                        title: $scope.errorTitle,
+                        type: 'danger',
+                        content: $scope.errorMsg,
+                        container: '#alert',
+                        dismissable: false,
+                        show: true
+                    });
+                case 500:
+                    $scope.myAlert = $alert({
+                        title: $scope.errorTitle,
+                        type: 'danger',
+                        content: 'Internal server error.',
+                        container: '#alert',
+                        dismissable: false,
+                        show: true
+                    })
+                    break;
+            }
+            $scope.loading = false;
+        });
 
     }
-
 ]);
+
+
