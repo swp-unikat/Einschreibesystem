b6dfbd613203df69891b25703f689606c8cf562d
Stefan Heringklee <stefan.heringklee@tu-ilmenau.de>
2016-06-13 14:03:18 +0200

    updated blacklistCtrl.js restService.js adminBlacklist.html

--- web/components/controllers/blacklistCtrl.js
+++ web/components/controllers/blacklistCtrl.js
@@ -2,9 +2,26 @@ var mainAppCtrls = angular.module("mainAppCtrls");
 /**
  *
  */
-mainAppCtrls.controller('BlacklistCtrl',['$scope',
-    function($scope) {
-        
-    }
+    mainAppCtrls.controller('BlacklistCtrl', ['$scope', "Participants",'$alert','$modal',
 
+        function ($scope, Participants, $alert,$modal) {
+
+
+            var loadBlacklist = function () {
+                $scope.loading = true;
+                Participants.getblacklistall()
+                    .$promise.then(function (value) {
+                    $scope.userdata = value;
+                    $scope.loading = false;
+
+                }, function (httpResponse) {
+                    $scope.loading = false;
+                });
+            };
+            loadBlacklist();
+
+
+        }
+            
+            
 ]);
