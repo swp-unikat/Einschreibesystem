b832d0c878414ef9d7a3b205be7497769385e24b
MarcoHanisch <marco.hanisch@tu-ilmenau.de>
2016-06-15 00:10:51 +0200

    Update Invitation.php
    PHPDoc

--- src/Core/EntityBundle/Entity/Invitation.php
+++ src/Core/EntityBundle/Entity/Invitation.php
@@ -10,7 +10,10 @@ namespace Core\EntityBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
-/** @ORM\Entity */
+/**
+ * this class provides the entitys and methods for the invitation
+ * @ORM\Entity 
+*/
 class Invitation
 {
     /** @ORM\Id @ORM\Column(type="string", length=6) */
@@ -27,24 +30,32 @@ class Invitation
      * @ORM\Column(type="boolean")
      */
     protected $sent =false;
-
+    /**
+     * function to construct an invitation
+     */
     public function __construct()
     {
         //generate identifier only once, here a 6 characters length code
         $this->code = substr(md5(uniqid(rand(), true)), 0, 6);
     }
-
+    /**
+     * function to get code of an invitaion
+     */
     public function  getCode()
     {
         return $this->code;
     }
 
-    
+    /**
+     * function to get sendstate of invitation
+     */
     public function isSent()
     {
         return $this->sent;
     }
-
+    /**
+     * function to send invitation
+     */
     public function send()
     {
         $this->sent = true;
