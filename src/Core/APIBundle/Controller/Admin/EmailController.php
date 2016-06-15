af2ae7eeb441204666a245df72b420ee66a22eb9
Leon Bergmann <leon.bergmann@sky-lab.de>
2016-06-15 03:05:03 +0200

    completed some actions

--- src/Core/APIBundle/Controller/Admin/EmailController.php
+++ src/Core/APIBundle/Controller/Admin/EmailController.php
@@ -54,18 +54,25 @@ class EmailController extends FOSRestController implements ClassResourceInterfac
      *
      * @return \Symfony\Component\HttpFoundation\Response
      * @Rest\View()
-     * @param $workshopID id of a workshop
+     * @param $workshopId int id of a workshop
+     * @param $request Request
      */
-    public function sendAction($workshopId)
+    public function postSendAction($workshopId,Request $request)
     {
-	    $workshopParticipants = $this->getDoctrine()->getManager()->getRepository("CoreEntityBundle:WorkshopParticipants")->findBy(['workshop'=> $workshopId]);
+        $renderTemplate = $this->get('twig')->createTemplate($request->get('content'));
 
-    }
-    /**
-     * function to set the state of a workshop on notify
-     * @param $workshopID id of a workshop
-     */
-    public function notifyAction($workshopId){
+        $workshopParticipants = $this->getDoctrine()->getManager()->getRepository("CoreEntityBundle:WorkshopParticipants")->findBy(['workshop'=> $workshopId]);
+
+        foreach ($workshopParticipants as $participant)
+        {
+            $message = \Swift_Message::newInstance()
+                ->setSubject($request->get('subject'))
+                ->setFrom('send@example.com')//unsure which email!
+                ->setTo($participant->getParticipant()->getEmail())
+                ->setBody($renderTemplate->render(['participant' => $participant,'workshop' => $participant->getWorkshop()]), 'text/html');
+            $this->get('mailer')->send($message);
+        }
         
     }
+
 }
