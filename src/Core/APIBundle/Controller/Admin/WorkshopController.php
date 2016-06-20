0a3756128ffac5b734b0a8a9d9d285f473e1f6d1
MarcoHanisch <marco.hanisch@tu-ilmenau.de>
2016-06-20 17:36:18 +0200

    Update WorkshopController.php

--- src/Core/APIBundle/Controller/Admin/WorkshopController.php
+++ src/Core/APIBundle/Controller/Admin/WorkshopController.php
@@ -1,8 +1,7 @@
 <?php
 /**
  * Created by IntelliJ IDEA.
- * User: Leon Bergmann
- * Company: SkyLab UG(haftungsbeschränkt) 
+ * Authors: Leon Bergmann, Martin Griebel, Marco Hanisch 
  * Date: 29.04.2016
  * Time: 16:44
  */
