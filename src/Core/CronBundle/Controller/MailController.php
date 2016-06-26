e6eb88396c805a1628360af556d955c9e852c853
Leon Bergmann <leon.bergmann@sky-lab.de>
2016-06-26 04:39:43 +0200

    improved email

--- src/Core/CronBundle/Controller/MailController.php
+++ src/Core/CronBundle/Controller/MailController.php
@@ -8,44 +8,72 @@
 
 namespace Core\CronBundle\Controller;
 
+use Symfony\Bridge\Monolog\Handler\SwiftMailerHandler;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Core\EntityBundle\Entity\Workshop;
 use Core\EntityBundle\Repository\WorkshopRepository;
 use Core\EntityBundle\Entity\EmailTemplate;
+use Doctrine\ORM\EntityManager;
+use Symfony\Bundle\MonologBundle\MonologBundle;
+
 /**
  * Class to load E-Mail Template and send E-Mail
  */
 class MailController extends Controller{
+
+    protected $em;
+    protected $twig;
+    protected $logger;
+    protected $mailer;
+
+    public function __construct(EntityManager $em,\Twig_Extension $twig,MonologBundle $logger,\Swift_Mailer $mailer)
+    {
+        $this->em		= $em;
+        $this->twig		= $twig;
+        $this->logger	= $logger;
+        $this->mailer  	= $mailer;
+    }
+
     /**
-     * function to load participants of a workshop 
+     * function to load participants of a workshop
      */
     public function run(){
         $count = 0;
         /* Load Workshops to get notified*/
-        $workshops = $this->getDoctrine()->getRepository("CoreEntityBundle:Workshop")->getWorkshopsForNotificationEmail();
+        $workshops = $this->em->getRepository("CoreEntityBundle:Workshop")->getWorkshopsForNotificationEmail();
+        if(!$workshops){
+            $this->logger->info("No Workshops to notify.");
+            return 0;
+        }
         foreach ($workshops as $id) {
             /* Load Workshop object */
-            $workshop = $this->getDoctrine()->getRepository("CoreEntityBundle:Workshop")->find($id['id']);
+            $workshop = $this->em->getRepository("CoreEntityBundle:Workshop")->find($id['id']);
             /* Load Workshop Participants*/
-            $participants = $this->getDoctrine()->getRepository("CoreEntityBundle:Workshop")->getParticipants($workshop->getId());
-            $count += $this->sendMail($participants,$workshop);
-            $workshop->setNotified(true);
+            $participants = $this->em->getRepository("CoreEntityBundle:Workshop")->getParticipants($workshop->getId());
+            if($participants){
+                $count += $this->sendMail($participants,$workshop);
+                $workshop->setNotified(true);
+                $this->em->persist($workshop);
+                $this->logger->info("Notified ".$count." participants of workshop: ".$workshop->getTitle());
+            }else{
+                $this->logger->info("No participants to notify for workshop: ".$workshop->getTitle());
+            }
         }
-
+        $this->em->flush();
         return $count;
     }
     /**
      * function to send a E-Mail to participants of a workshop
      * @param $participants array participant
-     * @param $workshop int workshop
+     * @param $workshop Workshop workshop
      * @return int
      */
     protected function sendMail($participants,$workshop){
         $counter = 0;
         /* Loading the default E-Mail template*/
-        $template = $this->getDoctrine()->getRepository("CoreEntityBundle:EmailTemplate")->find(1);
+        $template = $this->em->getRepository("CoreEntityBundle:EmailTemplate")->find(1);
         /* Creating Twig template from Database */
-        $renderTemplate = $this->get('twig')->createTemplate($template->getEmailBody());
+        $renderTemplate = $this->twig->createTemplate($template->getEmailBody());
         foreach ($participants as $participant){
             /* Sending E-Mail */
             $message = \Swift_Message::newInstance()
@@ -53,7 +81,7 @@ class MailController extends Controller{
                 ->setFrom($this->getParameter('email_sender'))
                 ->setTo($participant['email'])
                 ->setBody($renderTemplate->render(["workshop" => $workshop,"participant" => $participant]),'text/html');
-            $this->get('mailer')->send($message);
+            $this->mailer->send($message);
             $counter++;
         }
         return $counter;
