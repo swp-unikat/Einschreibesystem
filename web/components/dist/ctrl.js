f0d6b4e9a3c9ed2fe3ac6cf56e5033421f293b00
AhmetDurak7777 <ahmet.durak@tu-ilmenau.de>
2016-07-06 16:28:17 +0200

    update

--- web/components/dist/ctrl.js
+++ web/components/dist/ctrl.js
@@ -31,7 +31,7 @@ mainAppCtrls.controller('AdminCreateCtrl',['$scope', '$stateParams','$alert','$t
         //Get translations for errors and store in array
          var _translations = {};
         //Pass all required translation IDs to translate service
-        $translate(['PASSWORDS_IDENTICAL_ERROR', 'EMAIL', 'USERNAME', 'NEW_PASSWORD', 'REPEAT_PASSWORD'])
+        $translate(['PASSWORDS_IDENTICAL_ERROR', 'EMAIL', 'USERNAME', 'NEW_PASSWORD', 'REPEAT_PASSWORD', 'ALERT_CREATE_ADMIN_FAIL', 'ALERT_CREATE_ADMIN_SUCCESS'])
             .then(function(translations){
                 _translations = translations;
                 $scope.placeholder =  {
