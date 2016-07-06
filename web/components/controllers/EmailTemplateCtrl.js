abfc4ce2a10872d67b07f68d2fff08a94f00fc58
Stefan Heringklee <stefan.heringklee@tu-ilmenau.de>
2016-07-06 14:45:24 +0200

    updated EmailTemplateCtrl.js

--- web/components/controllers/EmailTemplateCtrl.js
+++ web/components/controllers/EmailTemplateCtrl.js
@@ -4,20 +4,28 @@
 var mainAppCtrls = angular.module("mainAppCtrls");
 /**
  * @ngdoc controller
-* @name mainAppCtrls.controller:EmailTemplateCtrl
-* @description Module containing all email templates
+ * @name mainAppCtrls.controller:EmailTemplateCtrl
+ * @description Module containing all email templates
  * @requires restSvscs.EmailTemplate
-*/
-mainAppCtrls.controller('EmailTemplateCtrl', ['$scope', "EmailTemplate",'$alert','$modal',
-    
-    function ($scope, EmailTemplate, $alert,$modal) {
+ */
+mainAppCtrls.controller('EmailTemplateCtrl', ['$scope', "EmailTemplate", '$alert', '$modal', '$translate',
+
+    function ($scope, EmailTemplate, $alert, $modal, $tramslate) {
+
+        //Get translations for errors and store in array
+        var _translations = {};
+        //Pass all required translation IDs to translate service
+        $translate(['ALERT_EMAILTEMPLATE_DELETE_SUCCESS', 'ALERT_EMAILTEMPLATE_DELETE_FAIL']).then(function (translations) {
+            _translations = translations;
+        });
+
         /**
          * @ngdoc function
          * @name mainAppCtrls.controller:EmailTemplateCtrl#loadTemplates
          * @methodOf mainAppCtrls.controller:EmailTemplateCtrl
          * @description Function loads the actual list of all email templates
          */
-        var loadTemplates = function() {
+        var loadTemplates = function () {
             $scope.loading = true;
             EmailTemplate.getAll()
                 .$promise.then(function (value) {
@@ -37,20 +45,27 @@ mainAppCtrls.controller('EmailTemplateCtrl', ['$scope', "EmailTemplate",'$alert'
          * @params {number} _id email template id, which should be removed
          */
         $scope.delete = function (_id) {
-            EmailTemplate.delete({id:_id}).$promise.then(function(httpResponse){
+            EmailTemplate.delete({id: _id}).$promise.then(function (httpResponse) {
                     $alert({
-                        title:'Success',
+                        title: 'Success',
                         type: 'success',
-                        container:'#alert',
+                        container: '#alert',
                         show: true,
                         dismissable: false,
-                        content: 'Successfully deleted',
+                        content: _translations.ALERT_EMAILTEMPLATE_DELETE_SUCCESS,
                         duration: 20
                     });
-                loadTemplates();
-            }
+                    loadTemplates();
+                }
                 , function (httpResponse) {
-                    alert('Error');
+                    $alert({
+                        title: '',
+                        type: 'danger',
+                        content: _translations.ALERT_EMAILTEMPLATE_DELETE_FAIL + ' (' + httpResponse.status + ')',
+                        container: '#alert',
+                        dismissable: false,
+                        show: true
+                    });
                 }
             )
 
