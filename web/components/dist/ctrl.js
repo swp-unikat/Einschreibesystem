47d9cc7b516ffcc0103b5eb4530b62e2d240c8a8
Stefan Heringklee <stefan.heringklee@tu-ilmenau.de>
2016-07-07 12:06:23 +0200

    updated editWorkshopTemplateCtrl.js

--- web/components/dist/ctrl.js
+++ web/components/dist/ctrl.js
@@ -1399,7 +1399,7 @@ mainAppCtrls.controller('EditWorkshopTemplateCtrl',['$scope','WorkshopTemplate',
                 });
                 error = true;
             }
-
+                var now = new Date();
             if($scope.workshop.start_at < now) {
                 $alert({
                     title:  _translations.TITLE_ERROR,
